db0f936aacef183bec6da63b611e19c59a017fc6
Your Name <khalidsaifullaha3@gmail.com>
2025-10-02 16:28:08 +0600

    Pagination & Searching

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -12,7 +12,10 @@ const createPost = async (req: Request, res: Response) => {
 
 const getAllPosts = async (req: Request, res: Response) => {
   try {
-    const result = await PostService.getAllPosts();
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const search = (req.query.search as string) || "";
+    const result = await PostService.getAllPosts({ page, limit, search });
     res.json(result);
   } catch (err) {
     res.status(500).json({ error: "Failed to fetch posts", details: err });
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -18,8 +18,34 @@ const createPost = async (payload: Prisma.PostCreateInput): Promise<Post> => {
   return result;
 };
 
-const getAllPosts = async () => {
-  const result = await prisma.post.findMany();
+const getAllPosts = async ({
+  page,
+  limit,
+  search,
+}: {
+  page: number;
+  limit: number;
+  search: string;
+}) => {
+  const skip = (page - 1) * limit;
+  const result = await prisma.post.findMany({
+    skip,
+    take: limit,
+    where: {
+      OR: [
+        {
+          title: {
+            contains: search,
+            mode: "insensitive",
+          },
+          content: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
   return result;
 };
 
