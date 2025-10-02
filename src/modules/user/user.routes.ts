27dd6457e609c33674c57a6f7382812c302b1756
Your Name <khalidsaifullaha3@gmail.com>
2025-10-02 12:11:32 +0600

    post create

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -9,4 +9,8 @@ router.get("/:id", UserController.getUserById);
 
 router.post("/", UserController.createUser);
 
+router.patch("/:id", UserController.updateUser);
+
+router.delete("/:id", UserController.deleteUser);
+
 export const userRouter = router;
